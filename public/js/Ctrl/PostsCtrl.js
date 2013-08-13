4c45a21485580eaf5cfe02a52f3ef6a84aceeea8
Jason Schwarzenberger <=>
2013-08-13 23:02:00 +1200

    add comments support, booyah!

--- public/js/Ctrl/PostsCtrl.js
+++ public/js/Ctrl/PostsCtrl.js
@@ -12,7 +12,7 @@ define(['angular',''], function(angular) {
 			var apiKey = 'AIzaSyD6waxLgpwHd0QXrExGwBMigr-01mQ4Lr4';
 			var googlePlusUrl = 'https://www.googleapis.com/plus/v1/people/107744372254752109523/activities/public?&key=' + apiKey + '&fields=items(title,published,updated,url,actor,verb,object)&callback=define';
 
-			$scope.posts = [];
+			$scope.Posts = [];
 
 			$scope.loadGooglePlus = function() {
 				require([googlePlusUrl], function(data) {
@@ -20,6 +20,24 @@ define(['angular',''], function(angular) {
 					$scope.Posts = data.items;
 					$scope.$apply();
 				});
+			};
+
+			$scope.LoadReplies = function(post) {
+				require([post.object.replies.selfLink + '?key=' + apiKey + '&callback=define'], function(data) {
+					console.log('replies', data.items);
+					$scope.Posts.map(function(p, i) {
+						if (p.url == post.url) {
+							p.object.replies.items = data.items;
+						}
+						return p;
+					});
+					$scope.$apply();
+					console.log('posts', $scope.Posts);
+				});
+			};
+
+			$scope.RepliesLoaded = function(post) {
+				return angular.isArray(post.object.replies.items);
 			};
 
 			var init = function() {
